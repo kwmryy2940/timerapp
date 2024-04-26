c71e80c110f5121283bfca332a5bca81e7162cf2
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-04-26 16:37:02 +0900

    デプロイ用の設定を追加

--- vue-frontend/vite.config.js
+++ vue-frontend/vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:"/timerapp",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+    outDir:"./dist"
+  },
 })
