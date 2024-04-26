b4cced3956f932397b29e3dc9c8a128f17190a4e
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-04-26 10:23:44 +0900

    Vuetify導入

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,9 +1,37 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import router from "./router";
 import App from './App.vue'
 
+// Vuetify のインポートを追加 **********/
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const myVuetifyLightTheme = {
+  dark: false,
+  colors: {
+    primary: "#4DB6AC", // プライマリカラー
+    secondary: "#b0bec5", // セカンダリカラー
+    accent: "#6D4C41", // アクセントカラー
+    error: "#b71c1c",
+  },
+};
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'myVuetifyLightTheme',
+    themes: {
+      myVuetifyLightTheme,
+    },
+  },
+});
+
 const app = createApp(App);
+app.use(vuetify);
 app.use(router);
 app.mount("#app");
 
--- vue-frontend/src/views/Home.vue
+++ vue-frontend/src/views/Home.vue
@@ -2,6 +2,8 @@
 
 <template>
   <v-app>
-    <h1>Timer App</h1>
+    <v-container fluid class="d-flex align-center justify-center">
+      <h1>Timer App</h1>
+    </v-container>
   </v-app>
 </template>
