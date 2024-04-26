1a8c7d97bf900f61a16588518c578ef2ed6a3699
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-04-26 12:25:42 +0900

    ルーティングファイル作成、Vuetify導入、ボタン、テキスト配置

---
+++ vue-frontend/src/components/Timer.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container fluid class="d-flex align-center justify-center">
+    <v-sheet>
+      <v-row>
+        <v-col>
+          <v-select
+            v-model="selectedHours"
+            label="h"
+            :items="hours"
+            return-object
+          >
+            <template v-slot:selection="{ item }">{{ item.value }}</template>
+          </v-select>
+        </v-col>
+        <v-col>
+          <v-select
+            v-model="selectedMinutes"
+            label="min"
+            :items="minutes"
+            return-object
+          >
+            <template v-slot:selection="{ item }">{{ item.value }}</template>
+          </v-select>
+        </v-col>
+        <v-col>
+          <v-select
+            v-model="selectedSeconds"
+            label="sec"
+            :items="seconds"
+            return-object
+          >
+            <template v-slot:selection="{ item }">{{ item.value }}</template>
+          </v-select>
+        </v-col>
+      </v-row>
+    </v-sheet>
+  </v-container>
+  <v-container fluid class="d-flex align-center justify-center">
+      <v-btn >Start</v-btn>
+  </v-container>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const selectedHours = ref(0);
+const hours = ref([]);
+
+const selectedMinutes = ref(0);
+const minutes = ref([]);
+
+const selectedSeconds = ref(0);
+const seconds = ref([]);
+
+for (let i = 0; i <= 72; i++) {
+  hours.value.push(i);
+}
+
+for (let i = 0; i <= 59; i++) {
+  minutes.value.push(i);
+  seconds.value.push(i);
+}
+</script>
--- vue-frontend/src/views/Home.vue
+++ vue-frontend/src/views/Home.vue
@@ -1,9 +1,14 @@
-<script setup></script>
+<script setup>
+import Timer from '../components/Timer.vue';
+</script>
 
 <template>
   <v-app>
     <v-container fluid class="d-flex align-center justify-center">
       <h1>Timer App</h1>
     </v-container>
+    <v-main>
+      <Timer/>
+    </v-main>
   </v-app>
 </template>
